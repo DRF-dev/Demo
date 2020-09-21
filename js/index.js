94cf5e0d4cfbadf1283dbc702d87816a120dc7d9
drf <dany.francisco@outlook.com>
2020-09-21 16:09:52 +0200

    feat(messagesBot): add bot that can tell the date

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const form = document.getElementById('form')
 const input = document.getElementById('message_input')
 const messageZone = document.getElementById('messages')
-const notifBotForHour = document.getElementsByClassName('number_of_notif')[0]
 
 const date = new Date()
 const getHour = date.getHours()
@@ -10,8 +9,14 @@ const getDay = date.getDate()
 const getMonth = date.getMonth()
 const getYear = date.getFullYear()
 
-const rgxVerifyHour = /hour/
+const notifBotForHour = document.getElementsByClassName('number_of_notif')[0]
 let numberNotifBotHour = 0;
+
+const rgxVerifyHour = /hour/
+const rgxVerifyDate = {
+  date: /date/,
+  day: /day/
+}
 
 const newMessage = () => {
   const actualValue = input.value
@@ -37,33 +42,39 @@ const newMessage = () => {
   return actualValue
 }
 
+const bodyMessageForBot = (message) => {
+  let responseHour = document.createElement('div')
+  responseHour.className = "bot-infos"
+
+  let botName = document.createElement('span')
+  botName.className = "name"
+  botName.textContent = "botForHour"
+
+  let botMsg = document.createElement('li')
+  botMsg.className = "bot"
+  botMsg.textContent = (message).toString()
+
+  let botMoment = document.createElement('span')
+  botMoment.className = "date-message"
+  botMoment.textContent = `${getDay < 10? `0${getDay}`:getDay}/${getMonth < 9? `0${getMonth+1}`:getMonth+1}/${getYear} ${getHour}h${getMinute < 10? `0${getMinute}`:getMinute}`
+
+  responseHour.appendChild(botName)
+  responseHour.appendChild(botMsg)
+  responseHour.appendChild(botMoment)
+
+  messageZone.appendChild(responseHour)
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  const newActValue = newMessage(e)
+  const inputValue = newMessage(e)
 
-  if (rgxVerifyHour.test(newActValue)) {
-
-    let responseHour = document.createElement('div')
-    responseHour.className = "bot-infos"
-
-    let botName = document.createElement('span')
-    botName.className = "name"
-    botName.textContent = "botForHour"
-
-    let botMsg = document.createElement('li')
-    botMsg.className = "bot"
-    botMsg.textContent = (`Il est ${getHour}h${getMinute < 10? `0${getMinute}`:getMinute}`).toString()
-
-    let botMoment = document.createElement('span')
-    botMoment.className = "date-message"
-    botMoment.textContent = `${getDay < 10? `0${getDay}`:getDay}/${getMonth < 9? `0${getMonth+1}`:getMonth+1}/${getYear} ${getHour}h${getMinute < 10? `0${getMinute}`:getMinute}`
-
-    responseHour.appendChild(botName)
-    responseHour.appendChild(botMsg)
-    responseHour.appendChild(botMoment)
-
-    messageZone.appendChild(responseHour)
-
+  if (rgxVerifyHour.test(inputValue)) {
+    bodyMessageForBot(`It is ${getHour}h${getMinute < 10? `0${getMinute}`:getMinute}`)
+    numberNotifBotHour++
+    notifBotForHour.textContent = numberNotifBotHour
+  } else if(rgxVerifyDate.date.test(inputValue) || rgxVerifyDate.day.test(inputValue)) {
+    bodyMessageForBot(`Today is the ${getDay < 10? `0${getDay}`:getDay}/${getMonth < 9? `0${getMonth+1}`:getMonth+1}/${getYear}`)
     numberNotifBotHour++
     notifBotForHour.textContent = numberNotifBotHour
   }
